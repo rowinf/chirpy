07f5dca024664049f72b7df1160e60e6922424f2
rowinf <robert.irwin@outlook.co.nz>
2023-12-07 08:16:14 +1300

    add reset handler

--- main.go
+++ main.go
@@ -1,24 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+type apiConfig struct {
+	fileServerHits int
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileServerHits = 0
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
+
 func main() {
 	port := "8080"
+
+	apiConfig := apiConfig{
+		fileServerHits: 0,
+	}
 	// Create a new ServeMux
 	mux := http.NewServeMux()
-
-	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	handler := apiConfig.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 
 	mux.Handle("/app/", handler)
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(http.StatusText(http.StatusOK)))
 	})
-	// Wrap the mux in a custom middleware for CORS
+	mux.HandleFunc("/metrics/", func(w http.ResponseWriter, _ *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Hits: %d", apiConfig.fileServerHits)
+	})
+	mux.HandleFunc("/reset/", apiConfig.handlerReset)
+	// Wrp the mux in a custom middleware for CORS
 	corsMux := addCorsHeaders(mux)
 
 	// Create a new HTTP server with the corsMux as the handler
@@ -34,13 +54,20 @@ func main() {
 	}
 }
 
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits = cfg.fileServerHits + 1
+		next.ServeHTTP(w, r)
+	})
+}
+
 // addCorsHeaders is a middleware function that adds CORS headers to the response.
 func addCorsHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set CORS headers
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 
 		// If it's a preflight request, respond with 200 OK
 		if r.Method == http.MethodOptions {
